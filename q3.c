bb91fa94cda77aeb7fc81d7e5b539d0104c1fdaf
siddhes07 <siddheshnj07@gmail.com>
2025-10-30 23:24:22 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int cp;
+    printf("enter cp");
+    scanf("%d",&cp);
+    int sp;
+    printf("enter sp");
+    scanf("%d",&sp);
+    if(sp>cp){
+        printf("profit");
+    }elseif(cp=sp){
+        print("no profit no loss")
+    }
+return0;
+
+}
